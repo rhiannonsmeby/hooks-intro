b8bbb130839e5230cb2c1a8089b6b69f2ae99b43
Rhiannon Smeby <smebyrhi@gmail.com>
2021-04-12 17:40:47 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+//example
+//Class component (without hooks)
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       answer: "Yes"
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Is state important to know? {this.state.answer}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// function App() {
+//   const [value] = useState("Yes")
+
+//   return (
+//     <div className="App">
+//       <p>Is state important to know? {value}</p>
+//     </div>
+//   );
+// }
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.state.count}</h1>
+//                 <button>Change!</button>
+//             </div>
+//         )
+//     }
+// }
+
+//counter example
 
 function App() {
+  //set counter to 0
+  let [count, setCount] = useState(0);
+  let [answer, setAnswer] = useState('No');
+
+  function isOddClick() {
+    setAnswer(prevAnswer => prevAnswer === 'No' ? prevAnswer = 'Yes' : prevAnswer = 'No')
+  }
+
+  function increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function twoX() {
+    setCount(prevCount => prevCount * 2);
+  }
+
+  function squared() {
+    setCount(prevCount => prevCount * prevCount);
+  }
+
+  function reset() {
+    setCount(prevCount => prevCount = 0)
+  }
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h1>{count}</h1>
+          <button onClick={increment}>increment</button>
+          <button onClick={decrement}>decrement</button>
+          <button onClick={twoX}>multiply by two</button>
+          <button onClick={squared}>squared</button>
+          <button onClick={reset}>reset</button>
+          <h1>{answer}</h1>
+          <button onClick={isOddClick}>is odd click?</button>
+      </div>
+  )
 }
 
 export default App;
+
+/**
+ * rather than passing multiple things in useState,create multiple versions of state
+ * e.g.
+ * const [count, setCount] = useState(0);
+ * const [answer, setAnswer] = useState("Yes")
+ */
